5fe58662eb0acd367c5bfdbf7a982e83e1f4fd88
SHRE MANDANI <shitalpmandani@gmail.com>
2022-01-28 23:06:37 +0530

    From Shrey

---
+++ anwser.js
@@ -0,0 +1,22 @@
+//   Q.1 The table with id="age-table". 
+tbl = document.getElementById("age-table")
+
+//   Q.2  All label elements inside that table (there should be 3 of them).
+lbl = document.querySelectorAll("label")
+
+//   Q.3   The first td in that table (with the word “Age”).
+rows = document.getElementsByTagName("td")
+rows[0]
+
+//   Q.4   The form with name="search".
+document.querySelector("form")
+
+// for finding input array and nodelist
+document.querySelector("input")
+document.querySelectorAll("input")
+
+//  Q.5   The first input in that form.
+document.querySelectorAll("input")[1]
+
+//  Q.6   The last input in that form.
+document.querySelectorAll("input")[8]
